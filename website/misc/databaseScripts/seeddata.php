ec06dc7e72af67e7127ce25f50cc58920c42d0b8
LukeRawlings <lukerawlings01@gmail.com>
2018-03-16 15:26:36 -0500

    altered some CSS

--- website/misc/databaseScripts/seeddata.php
+++ website/misc/databaseScripts/seeddata.php
@@ -29,21 +29,54 @@ if (mysqli_query($conn, $query))
 	echo "<p>Database is seeded with fake Image data.</p>";
 
 
-$query = "Insert into Category(CategoryName, Description, ImageId)
+$query = "Insert into Category(CategoryName, ImageId)
 values
-	('Christmas', 'Christmas style', 1),
-	('Birthday', 'Birthday Style', 2);";
+	('Birthday', 1),
+	('Rememberance', 2),
+	('Shower', 1),
+	('Thank You', 2),
+	('Thinking of You', 1),
+	('Wedding', 2),
+	('Bacherlorette', 1),
+	('Get Well', 2),
+	('Holiday', 1),
+	('Special Days', 2);";
 
 if (mysqli_query($conn, $query))
 	echo "<p>Database is seeded with fake Category data.</p>";
 	
 
-$query = "Insert into Subcategory(SubcategoryName, Description, CategoryId, ImageId)
+$query = "Insert into Subcategory(SubcategoryName, CategoryId, ImageId)
 Values
-	('Rudolph', 'The Reindeer', 1, 5),
-	('Santa', 'Fat Man', 1, 6),
-	('Toys', 'Childs Toys', 2, 7),
-	('Confetti', 'Confetti', 2, 8);";
+	('Girl Birthday', 1, 5),
+	('Boy Birthday', 1, 6),
+	('Adult Female Birthday', 1, 7),
+	('Adult Male Birthday', 1, 8),
+	('MileStone Birthday', 1, 5),
+	('Baby Shower', 3, 6),
+	('Bridal Shower', 3, 7),
+	('Gender Reveal', 3, 8),
+	('Wedding', 6, 5),
+	('Will you be my...', 6, 6),
+	('Fun', 7, 7),
+	('Adult Themed', 7, 8),
+	('Rememberance', 2, 7),
+	('General', 4, 7),
+	('Thinking of You', 5, 7),
+	('New Years Eve', 9, 7),
+	('Valentine's Day, 9, 7),
+	('Mardi Gras Day', 9, 7),
+	('St. Patrick's Day', 9, 7),
+	('Easter', 9, 7),
+	('July 4th', 9, 7),
+	('Memorial Day', 9, 7),
+	('Mother's Day, 9, 7),
+	('Father's Day, 9, 7),
+	('Teachers Appreciation', 10, 7),
+	('Boss's Day, 10, 7),
+	('Administration Appreciation', 10, 7),
+	('Nurse Appreciation', 10, 7),
+	('Graduation', 10, 7);";
 
 if (mysqli_query($conn, $query))
 	echo "<p>Database is seeded with fake Subcategory data.</p>";
