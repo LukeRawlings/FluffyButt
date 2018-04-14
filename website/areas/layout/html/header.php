f3ab78825e1eb3248e05c6a9609c48228147115c
jlmorrison3 <jmorr1996@hotmail.com>
2018-04-14 10:05:53 -0500

    Created the 3 information section at bottom of homepage, and updated some other files

--- website/areas/layout/html/header.php
+++ website/areas/layout/html/header.php
@@ -1,4 +1,4 @@
-<img class="kitty" src="images/kitty.jpg">
+<img class="kitty" src="images/kitty.png">
 <header class="text-center">
 	<a href="./">
 		<img src="images/FluffyButt_logoText.jpg" class="logo">
