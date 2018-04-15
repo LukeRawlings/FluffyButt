f34858e0d9178bbd29eaa3c510dc596ec9b8be24
Todd Roberts <fit2def.user@gmail.com>
2018-04-15 14:34:05 -0500

    added stuff

--- website/areas/layout/html/footer.php
+++ website/areas/layout/html/footer.php
@@ -2,8 +2,4 @@
     <div class="footer-banner"></div>	
     <span class="hug hug-left">&copy; <?php echo date("Y");?> Fluffybutt Cookies</span>
     <span class="hug hug-right">Site by Tech Entrepreneurship Club</span>
-    <a href="https://www.facebook.com/sugarcookies4u2/" class="facebook-link">
-        <img src="images/facebook.png" class="facebook-image" />
-        <span class="facebook-message">Follow us!</span>
-    </a>
 </footer>
