dacb9b30220e3ee5a1b0ac96b2765d6f21914403
Todd Roberts <fit2def.user@gmail.com>
2018-04-17 11:37:05 -0500

    added touch

--- website/areas/order/js/cookies-controller.js
+++ website/areas/order/js/cookies-controller.js
@@ -10,39 +10,47 @@
         $scope.cookieCanvas.redraw();
         var canvas = $scope.cookieCanvas.canvas;
 
-        canvas.touchstart = function(event){
-            console.log(event);
-        };
-
         canvas.onmousedown = function(event){
-            console.log(event);
-            var rect = canvas.getBoundingClientRect();
-            var mouseX = (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
-            var mouseY = (event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
             $scope.brush.painting = true;
-            $scope.brush.addPoint(mouseX, mouseY);
-            $scope.cookieCanvas.redraw();
+            paint(event, false);
         };
 
         canvas.onmousemove = function(event){
-            if($scope.brush.painting){
-                var rect = canvas.getBoundingClientRect();
-                var mouseX = (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
-                var mouseY = (event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
-                $scope.brush.addPoint(mouseX, mouseY, true);
-                $scope.cookieCanvas.redraw();
-            }
+            if($scope.brush.painting)
+                paint(event, true);
         };
 
         canvas.onmouseup = function(event){
             $scope.brush.painting = false;
         };
-        canvas.onmouseout = function(event){
+
+        canvas.addEventListener('touchstart', function(event){
+            $scope.brush.painting = true;
+            var touch = event.touches[0];
+            paint(touch, false);
+        });
+
+        canvas.addEventListener('touchmove', function(event){
+            var touch = event.touches[0];
+            if ($scope.brush.painting){
+                paint(touch, true);
+            }
+        });
+
+        canvas.addEventListener('touchend', function(){
             $scope.brush.painting = false;
-        };
-       
-        canvas.touchcancel = function(event){
+        });
+
+        canvas.addEventListener('touchcancel', function(){
             $scope.brush.painting = false;
+        });
+
+        var paint = function(event, dragging){
+            var rect = canvas.getBoundingClientRect();
+            var mouseX = (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+            var mouseY = (event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+            $scope.brush.addPoint(mouseX, mouseY, dragging);
+            $scope.cookieCanvas.redraw();
         };
 
     };
